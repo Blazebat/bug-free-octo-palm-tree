da9e027a50adc39097da1ae3c53fe89791485799
Blazebat <antangkharmellechloe@gmail.com>
2025-08-29 10:27:17 -0700

    Delete android directory

--- android/gradlew.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SET DIRNAME=%~dp0
-IF "%JAVA_HOME%" == "" (
-    SET JAVACMD=java
-) ELSE (
-    SET JAVACMD=%JAVA_HOME%\bin\java
-)
-SET CLASSPATH=%DIRNAME%gradle\wrapper\gradle-wrapper.jar
-"%JAVACMD%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
